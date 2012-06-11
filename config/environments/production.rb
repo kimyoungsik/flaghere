ef64f475e16f3dbf7891882b4a99cf276d1f3113
Kim Youngsik <dean4208@gmail.com>
2012-06-11 16:04:30 +0900

    config complile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,7 +47,7 @@ Flaghere::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
    # config.assets.precompile += %w( application.css )
-  # config.assets.compile = true
+  config.assets.compile = true
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
 
