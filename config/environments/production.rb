d0ef0c18459f17281198ae7bef439b2e0d66ead6
Kim Youngsik <dean4208@gmail.com>
2012-06-11 22:52:14 +0900

    production email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,4 +66,6 @@ Flaghere::Application.configure do
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
   # i18n
   config.i18n.locale = :ko
+  
+  config.action_mailer.default_url_options = { :host => 'mangomountain.kr' }
 end
