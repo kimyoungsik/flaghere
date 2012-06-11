be5ba105eebe82ab1ded1277546d3ad6fde5b89c
Kim Youngsik <dean4208@gmail.com>
2012-06-12 00:46:18 +0900

    phone test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   before_filter :authenticate_user!
   before_filter :noname_user, :except => [:destroy]
   before_filter :facebook_auth, :except => [:destroy]
+  before_filter :prepare_for_mobile
   def noname_user
     if user_signed_in? and (current_user.first_name.blank? or current_user.last_name.blank?)
       redirect_to gettingstarted_step1_path
@@ -15,6 +16,18 @@ class ApplicationController < ActionController::Base
     end
   end
   
+  private  
+  def mobile_device?  
+    if session[:mobile_param]  
+      session[:mobile_param] == "1"  
+    else  
+      request.user_agent =~ /Mobile|webOS/  
+    end  
+  end  
+  helper_method :mobile_device?  
   
+  def prepare_for_mobile  
+    session[:mobile_param] = params[:mobile] if params[:mobile]  
+  end
 
 end
