dafb3cb5ed21586923852ad00d968a69e3f5b44f
Kim Youngsik <dean4208@gmail.com>
2012-06-11 14:26:12 +0900

    precompile cancle

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,7 +47,7 @@ Flaghere::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
-  config.assets.compile = true
+  # config.assets.compile = true
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
 
